a36824e1240aed541291237a99e44be338e73195
Bournane Mohamed <mbournane@gmail.com>
2023-06-13 22:01:26 +0200

    Quest Dictionary

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Dictionary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<char,string> dictionary = new Dictionary<char,string>();
+        dictionary['a'] = "Automobile";
+        dictionary['b'] = "Bateau";
+        dictionary['c'] = "Camion";
+
+        dictionary.Remove('c');
+        for (int i = 0; i < dictionary.Count; i++)
+                    Console.WriteLine("Key: {0}, Value: {1}", dictionary.ElementAt(i).Key, dictionary.ElementAt(i).Value);                                                    
+                                                    
+
+
+    }
+}
